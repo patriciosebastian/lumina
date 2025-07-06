4cb70094277196e686c29505564c3e17ddf8d5c6
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-06 13:54:15 -0700

    defined JournalFactory

--- web/database/factories/JournalFactory.php
+++ web/database/factories/JournalFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Journal;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,6 +12,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class JournalFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Journal::class;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -17,7 +26,63 @@ class JournalFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->sentence(3, true),
+            'description' => $this->faker->optional(0.7)->paragraph(),
+            'user_id' => User::factory(),
+            'created_at' => $this->faker->dateTimeBetween('-6 months', 'now'),
+            'updated_at' => function (array $attributes) {
+                return $this->faker->dateTimeBetween($attributes['created_at'], 'now');
+            },
         ];
+    }
+
+    /**
+     * Indicate that the journal should have a specific user.
+     */
+    public function forUser(User $user): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'user_id' => $user->id,
+        ]);
+    }
+
+    /**
+     * Indicate that the journal should have a specific name.
+     */
+    public function withName(string $name): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'name' => $name,
+        ]);
+    }
+
+    /**
+     * Indicate that the journal should not have a description.
+     */
+    public function withoutDescription(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'description' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the journal should be created recently.
+     */
+    public function recent(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'created_at' => $this->faker->dateTimeBetween('-1 week', 'now'),
+        ]);
+    }
+
+    /**
+     * Indicate that the journal should be created long ago.
+     */
+    public function old(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'created_at' => $this->faker->dateTimeBetween('-2 years', '-6 months'),
+        ]);
     }
 }
