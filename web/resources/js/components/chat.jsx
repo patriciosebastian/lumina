fd2d37a3f218a7f1e952532b8861178b558bea5b
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-12 20:28:57 -0700

    progress in chat.jsx

--- web/resources/js/components/chat.jsx
+++ web/resources/js/components/chat.jsx
@@ -1,9 +1,29 @@
-import SalosCard from './ui/salosCard';
+import { useState } from 'react';
+import MessageCard from './messageCard';
 
 export default function Chat() {
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    const handleSendMessage = () => {
+        if (message.trim()) {
+          setMessages(prev => [...prev, { text: message, role: 'user' }]);
+          setMessage('');
+        }
+    };
+
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value);
+    };
+
   return (
     <>
-        <SalosCard />
+        <MessageCard
+            currentMessage={message}
+            messages={messages}
+            onSendMessage={handleSendMessage}
+            onMessageChange={handleMessageChange}
+        />
     </>
   );
 }
