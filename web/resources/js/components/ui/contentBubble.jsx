c9dc0bc06a6d9d32f388ce6927f2a15cd7f76f12
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-12 23:32:17 -0700

    mobile responsive stles to contentBubble

--- web/resources/js/components/ui/contentBubble.jsx
+++ web/resources/js/components/ui/contentBubble.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ContentBubble({ children }) {
   return (
-    <div className="w-[34.375rem] text-lg font-normal leading-normal text-primary-200 -tracking-[2%] bg-[#00AFFF14] rounded-2xl border border-primary-950 px-5 py-6">
+    <div className="w-[70%] text-lg font-normal leading-normal text-primary-200 -tracking-[2%] bg-[#00AFFF14] rounded-2xl border border-primary-950 px-5 py-6 md:w-[60%] lg:w-[34.375rem]">
         {children}
     </div>
   );
