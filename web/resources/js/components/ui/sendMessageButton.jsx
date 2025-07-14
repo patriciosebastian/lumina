de7a73a4704c76773176996a27bda9f931e49cee
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-13 22:33:27 -0700

    forgot to add className prop to message and voice buttons

--- web/resources/js/components/ui/sendMessageButton.jsx
+++ web/resources/js/components/ui/sendMessageButton.jsx
@@ -1,11 +1,11 @@
 import { ArrowUpIcon } from 'lucide-react';
 import { Button } from './button';
 
-export default function SendMessageButton({ onSendMessage }) {
+export default function SendMessageButton({ onSendMessage, className = '' }) {
   return (
     <Button
         variant={'salosSecondary'}
-        className="w-[4.5rem] h-full px-4 py-5 rounded-2xl border-none z-0 before:content-[''] before:absolute before:z-[-1] before:inset-0 before:p-[1px] before:rounded-2xl before:bg-gradient-to-r before:from-purple-700 before:to-cyan-300 before:[mask:linear-gradient(var(--color-primary-500)_0_0)_exclude,_linear-gradient(#000_0_0)_content-box]"
+        className={`w-[4.5rem] h-full px-4 py-5 rounded-2xl border-none z-0 before:content-[''] before:absolute before:z-[-1] before:inset-0 before:p-[1px] before:rounded-2xl before:bg-gradient-to-r before:from-purple-700 before:to-cyan-300 before:[mask:linear-gradient(var(--color-primary-500)_0_0)_exclude,_linear-gradient(#000_0_0)_content-box] ${className}`}
         onClick={onSendMessage}
     >
         <ArrowUpIcon className="size-8" />
--- web/resources/js/components/ui/voiceButton.jsx
+++ web/resources/js/components/ui/voiceButton.jsx
@@ -1,11 +1,11 @@
 import { Button } from './button';
 import { MicIcon } from 'lucide-react';
 
-export default function VoiceButton() {
+export default function VoiceButton({ className = '' }) {
   return (
     <Button
         variant={'salosSecondary'}
-        className="w-[4.5rem] h-full px-4 py-5 rounded-2xl border-none z-0 before:content-[''] before:absolute before:z-[-1] before:inset-0 before:p-[1px] before:rounded-2xl before:bg-gradient-to-r before:from-purple-700 before:to-cyan-300 before:[mask:linear-gradient(var(--color-primary-500)_0_0)_exclude,_linear-gradient(#000_0_0)_content-box]"
+        className={`w-[4.5rem] h-full px-4 py-5 rounded-2xl border-none z-0 before:content-[''] before:absolute before:z-[-1] before:inset-0 before:p-[1px] before:rounded-2xl before:bg-gradient-to-r before:from-purple-700 before:to-cyan-300 before:[mask:linear-gradient(var(--color-primary-500)_0_0)_exclude,_linear-gradient(#000_0_0)_content-box] ${className}`}
     >
         <MicIcon className="size-8" />
     </Button>
