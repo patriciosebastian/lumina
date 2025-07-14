f946b9700b7d899bc3228f0cf8edd52b7b699ad1
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-14 07:34:56 -0700

    modifiable icon styles in send message and voice buttons

--- web/resources/js/components/ui/sendMessageButton.jsx
+++ web/resources/js/components/ui/sendMessageButton.jsx
@@ -1,14 +1,14 @@
 import { ArrowUpIcon } from 'lucide-react';
 import { Button } from './button';
 
-export default function SendMessageButton({ onSendMessage, className = '' }) {
+export default function SendMessageButton({ onSendMessage, className = '', iconClasses = '' }) {
   return (
     <Button
         variant={'salosSecondary'}
         className={`w-[4.5rem] h-full px-4 py-5 rounded-2xl border-none z-0 before:content-[''] before:absolute before:z-[-1] before:inset-0 before:p-[1px] before:rounded-2xl before:bg-gradient-to-r before:from-purple-700 before:to-cyan-300 before:[mask:linear-gradient(var(--color-primary-500)_0_0)_exclude,_linear-gradient(#000_0_0)_content-box] ${className}`}
         onClick={onSendMessage}
     >
-        <ArrowUpIcon className="size-8" />
+        <ArrowUpIcon className={`size-8 ${iconClasses}`} />
     </Button>
   );
 }
