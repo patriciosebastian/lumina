8423a100996397d926e7a539546956631e343192
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-15 23:29:35 -0700

    WIP on saving user and guest chat messages

--- web/app/Http/Controllers/ChatController.php
+++ web/app/Http/Controllers/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\MessageRole;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
@@ -26,11 +27,23 @@ class ChatController extends Controller
         ]);
 
         $user = $request->user();
+        $message = $request->input('message');
 
         if (!$user) {
             $guestMessages = session('guest_messages', []);
-            $guestMessages[] = $request->input('message');
+            // \Log::info('Storing guest message in session. Message: ' . $message);
+            $guestMessages[] = $message;
             session(['guest_messages' => $guestMessages]);
+        } else {
+            // Account for saving to existing chats: I need to receive the chat information if the message is being sent from within an existing chat. (ref to my notes)
+
+            $user->chats()->create([
+                'name' => substr($message, 0, 30), // TODO: Change this to a more meaningful name
+                'user_id' => $user->id,
+            ])->messages()->create([
+                'content' => $message,
+                'role' => MessageRole::USER,
+            ]);
         }
 
         return Redirect::route('chat')->with('success', 'Message sent successfully');
