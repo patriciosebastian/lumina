e40c30ba661f0641d895dcd6cdeda05b66149555
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-17 16:39:05 -0700

    updated sidebar group label name based on route params

--- web/resources/js/components/nav-main.tsx
+++ web/resources/js/components/nav-main.tsx
@@ -3,10 +3,20 @@ import { Link, usePage } from '@inertiajs/react';
 
 export function NavMain({ items = [] }: { items: [] }) {
     const page = usePage();
+    const currentPath = window.location.pathname;
+    const currentParams = new URLSearchParams(window.location.search);
+
+    const getSidebarLabel = (): string => {
+        if (currentPath === '/chat' && currentParams.get('journal') === 'true') {
+            return 'Journals';
+        }
+
+        return 'Chats';
+    }
 
     return (
         <SidebarGroup className="px-2 py-0">
-            <SidebarGroupLabel>Chats</SidebarGroupLabel>
+            <SidebarGroupLabel>{getSidebarLabel()}</SidebarGroupLabel>
             <SidebarMenu>
                 {items.map((item) => (
                     <SidebarMenuItem key={item.id}>
