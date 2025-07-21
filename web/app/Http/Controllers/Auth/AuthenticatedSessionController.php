2fb48b288e3f3539b629acdc9fea3c9064bc58b2
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-20 21:46:03 -0700

    fixed reference to named route

--- web/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ web/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('chat', absolute: false));
+        return redirect()->intended(route('chat.index', absolute: false));
     }
 
     /**
