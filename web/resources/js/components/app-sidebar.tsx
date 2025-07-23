eed68a313a66c696797d59b8aae36f9173e53355
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-23 13:25:59 -0700

    added new chat/journal button

--- web/resources/js/components/app-sidebar.tsx
+++ web/resources/js/components/app-sidebar.tsx
@@ -7,6 +7,7 @@ import { Link } from '@inertiajs/react';
 import SalosLogo from './ui/salosLogo';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { useRoute } from 'ziggy-js';
+import { Edit } from 'lucide-react';
 
 const footerNavItems: NavItem[] = [
     // {
@@ -24,11 +25,21 @@ const footerNavItems: NavItem[] = [
 export function AppSidebar({ data = [] }: { data?: [] }) {
     const isMobile = useIsMobile();
     const route = useRoute();
+    const isChatRoute = route().current('chat.*');
+
+    const getRouteName = (): string => {
+        return isChatRoute ? 'chat.index' : 'journal.index';
+    }
+
+    const getLabel = (): string => {
+        return isChatRoute ? 'New Chat' : 'New Journal';
+    }
 
     return (
         <div className="relative">
             <div className={`${!isMobile ? "bg-transparent rounded-2xl before:rounded-2xl before:content-[''] before:absolute before:z-[1] before:inset-2 before:p-[1px] before:bg-gradient-to-r before:from-purple-700 before:to-cyan-300 before:[mask:linear-gradient(var(--color-primary-500)_0_0)_exclude,_linear-gradient(#000_0_0)_content-box]" : ''}`}>
                 <Sidebar
+                    collapsible="icon"
                     className="border-none"
                     mobileClasses="border-none border-r-0 bg-background rounded-2xl before:rounded-2xl before:content-[''] before:absolute before:z-[1] before:inset-0 before:p-[0px] before:bg-gradient-to-r before:from-purple-700 before:to-cyan-300 before:[mask:linear-gradient(var(--color-primary-500)_0_0)_exclude,_linear-gradient(#000_0_0)_content-box]"
                 >
@@ -49,6 +60,19 @@ export function AppSidebar({ data = [] }: { data?: [] }) {
                     </SidebarHeader>
 
                     <SidebarContent>
+                        <SidebarMenuButton
+                            asChild
+                            className="w-auto pl-2 my-2 mx-2 hover:bg-primary-950/70"
+                        >
+                            <Link
+                                href={route(getRouteName())}
+                                prefetch
+                                className="flex items-center gap-4 px-2 mx-2"
+                            >
+                                <Edit className="text-primary-500" />
+                                <span>{getLabel()}</span>
+                            </Link>
+                        </SidebarMenuButton>
                         <NavMain items={data} />
                     </SidebarContent>
 
