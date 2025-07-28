efa41b19a2f0da92a506fe97e9dda4f65dc99259
Patricio Salazar <psebastiansalazar@gmail.com>
2025-07-28 07:27:11 -0700

    updated pricing cards data

--- web/resources/js/data/static/pricingCards.js
+++ web/resources/js/data/static/pricingCards.js
@@ -25,28 +25,28 @@ const pricingCards = [
             "Daily devotional messages."
         ],
         monthlyPrice: 4.99,
-        yearlyPrice: 44.99,
+        yearlyPrice: 49.99,
         monthlyPriceId: 'price_1RofbU338HSujneJq5VBjLOn',
         yearlyPriceId: 'price_1Ronkv338HSujneJ4x2OKKEN',
         buttonText: "Get Started Now",
         popular: true
     },
-    // {
-    //     badgeText: "Advanced Access",
-    //     title: "Premium",
-    //     subtitle: "Essential features for startups and small teams.",
-    //     features: [
-    //         "All Standard features without no limitations.",
-    //         "Advanced Recall: Deeper memory for evolving conversations.",
-    //         "Personalized spiritual journaling & progress tracking."
-    //     ],
-    //     monthlyPrice: 19.99,
-    //     yearlyPrice: 99.99,
-    //     monthlyPriceId: '',
-    //     yearlyPriceId: '',
-    //     buttonText: "Get Started Now",
-    //     popular: false
-    // }
+    {
+        badgeText: "Advanced Access",
+        title: "Premium",
+        subtitle: "Essential features for startups and small teams.",
+        features: [
+            "All Standard features without no limitations.",
+            "Advanced Recall: Deeper memory for evolving conversations.",
+            "Personalized spiritual journaling & progress tracking."
+        ],
+        monthlyPrice: 9.99,
+        yearlyPrice: 89.99,
+        monthlyPriceId: 'price_1RpgEr338HSujneJWTc6lE6r',
+        yearlyPriceId: 'price_1RpgFR338HSujneJkmJxG6Ip',
+        buttonText: "Get Started Now",
+        popular: false
+    }
 ];
 
 export default pricingCards;
