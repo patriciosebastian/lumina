34b17f8f0f1fa5d1958a5f84d61a66bfbbda7707
Patricio Salazar <psebastiansalazar@gmail.com>
2025-09-15 01:39:13 -0700

    missed these two

---
+++ web/resources/js/components/centeredBannerCard.jsx
@@ -0,0 +1,42 @@
+import { Badge } from './ui/badge';
+import SalosCard from './ui/salosCard';
+import AIBubbles from '../../images/AI_Bubble.svg';
+
+export default function CenteredBannerCard({
+    className = '',
+    badgeClasses = '',
+    img = AIBubbles,
+    imgWidth = 60,
+    imgHeight = 60,
+    imgClasses = '',
+    imgAltText = 'Icon',
+    titleClasses = '',
+    title,
+    contentClasses = '',
+    children,
+}) {
+  return (
+    <SalosCard className={`max-lg:size-auto w-[60rem] h-[18.875rem] p-6 gap-4 max-w-[960px] rounded-[1.25rem] before:rounded-[1.25rem] lg:p-10 ${className}`}>
+        <SalosCard.Header className="gap-y-8">
+            <Badge
+                variant={'salosPrimary'}
+                className={`mx-auto w-[3.75rem] h-[3.75rem] max-h-none p-3.5 border rounded-[1.125rem] before:rounded-[1.125rem] flex justify-center items-center shadow-none lg:w-20 lg:h-20 lg:p-[1.125rem] ${badgeClasses}`}
+            >
+                <img
+                    src={img}
+                    width={imgWidth}
+                    height={imgHeight}
+                    className={`w-full object-cover object-center rounded-2xl min-w-8 ${imgClasses}`}
+                    alt={imgAltText}
+                />
+            </Badge>
+            <SalosCard.Header.Title className={`text-center !text-[1.75rem] leading-[110%] !mb-0 ${titleClasses}`}>
+                {title}
+            </SalosCard.Header.Title>
+        </SalosCard.Header>
+        <SalosCard.Content className={`text-sm text-center text-balance mx-auto leading-5 ${contentClasses}`}>
+            {children}
+        </SalosCard.Content>
+    </SalosCard>
+  );
+}
---
+++ web/resources/js/components/header.jsx
@@ -0,0 +1,22 @@
+import Logo from '../../images/Salos.svg';
+import { Switch } from '@/components/ui/switch';
+
+const Header = ({ enabled, setEnabled, switchText }) => {
+
+    return <div className="relative h-auto min-h-[100px] flex justify-between items-center px-8 py-4 mb-4">
+        <div>
+            <img
+                src={Logo}
+                width={116}
+                height={32}
+                alt="SALOS Logo"
+            />
+        </div>
+        <div className="relative bg-linear-to-b from-[#54C0F100] to-[#54C0F133] flex items-center justify-between px-4 w-[181px] h-[48px] gradient-border-mask rounded-[68px] text-white cursor-pointer">
+            <span className="text-[#54C0F1] text-sm">{switchText}</span>
+            <Switch checked={enabled} onCheckedChange={setEnabled} />
+        </div>
+    </div>;
+};
+
+export default Header;
