01fdf59023354ddca99e378dabe06c587a71e098
Patricio Salazar <psebastiansalazar@gmail.com>
2026-05-15 18:57:00 -0700

    added ssr.tsx

---
+++ web/resources/js/ssr.tsx
@@ -0,0 +1,36 @@
+import { createInertiaApp } from '@inertiajs/react';
+import createServer from '@inertiajs/react/server';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import ReactDOMServer from 'react-dom/server';
+import { type RouteName, route } from 'ziggy-js';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Lumina';
+
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: ReactDOMServer.renderToString,
+        title: (title) => title || appName,
+        resolve: (name) => resolvePageComponent(
+            [
+                `./pages/${name}.tsx`,
+                `./pages/${name}.jsx`,
+            ],
+            import.meta.glob('./pages/**/*.{tsx,jsx}'),
+        ),
+        setup: ({ App, props }) => {
+            /* eslint-disable */
+            // @ts-expect-error
+            global.route<RouteName> = (name, params, absolute) =>
+                route(name, params as any, absolute, {
+                    // @ts-expect-error
+                    ...page.props.ziggy,
+                    // @ts-expect-error
+                    location: new URL(page.props.ziggy.location),
+                });
+            /* eslint-enable */
+
+            return <App {...props} />;
+        },
+    }),
+);
